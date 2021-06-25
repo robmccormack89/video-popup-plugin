09c1e9400d911570208a97eaae6fb15851bcc6b8
robmccormack89 <robertmccormack147@gmail.com>
2021-06-25 12:21:02 +0100

    vendor added

---
+++ vendor/composer/installed.php
@@ -0,0 +1,110 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'f646d6023d545db3b8362882aea5472356e52ae7',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f646d6023d545db3b8362882aea5472356e52ae7',
+    ),
+    'altorouter/altorouter' => 
+    array (
+      'pretty_version' => 'v1.2.0',
+      'version' => '1.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '39c50092470128c12284d332bb57f306bb5b58e4',
+    ),
+    'asm89/twig-cache-extension' => 
+    array (
+      'pretty_version' => '1.4.0',
+      'version' => '1.4.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '13787226956ec766f4770722082288097aebaaf3',
+    ),
+    'composer/installers' => 
+    array (
+      'pretty_version' => 'v1.11.0',
+      'version' => '1.11.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ae03311f45dfe194412081526be2e003960df74b',
+    ),
+    'roundcube/plugin-installer' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'shama/baton' => 
+    array (
+      'replaced' => 
+      array (
+        0 => '*',
+      ),
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.23.0',
+      'version' => '1.23.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '46cd95797e9df938fdd2b03693b5fca5e64b01ce',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.23.0',
+      'version' => '1.23.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '2df51500adbaebdc4c38dea4c89a2e131c45c8a1',
+    ),
+    'timber/timber' => 
+    array (
+      'pretty_version' => '1.18.2',
+      'version' => '1.18.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '18766d1af8650ca919534cc497e7f0e8d82423a3',
+    ),
+    'twig/twig' => 
+    array (
+      'pretty_version' => 'v2.14.6',
+      'version' => '2.14.6.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '27e5cf2b05e3744accf39d4c68a3235d9966d260',
+    ),
+    'upstatement/routes' => 
+    array (
+      'pretty_version' => '0.5',
+      'version' => '0.5.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '3267d28be0a73f197087d58384e1a358d85671b6',
+    ),
+  ),
+);
